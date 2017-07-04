84839c7d87453c74d6a370f1ea1a4eedacf0cc23
jimchenhub <935432685@qq.com>
2017-07-04 22:20:45 +0800

    Com

--- survival_analysis.R
+++ survival_analysis.R
@@ -20,30 +20,6 @@ t3<-as.data.frame(cbind(sample,t2))
 #??os??os_ind??cli????????ȡ??��
 cli_os<-data.frame(cli$sampleID,cli$X_OS,cli$X_OS_IND)
 cli_os<-na.omit(cli_os)
-#?ѻ???????t3???ٴ???Ϣcli_os?????????ϲ???һ??
-#fix(t3)#??t3?е?????sample??ΪsampleID
-colnames(t3)[1]<-"sampleID"
-colnames(cli_os)[1]<-"sampleID"
-total<-merge(t3,cli_os,by="sampleID")#temp<-match(t3$sampleID,cli$sampleID,nomatch=NA)
-write.table(total, "bindData.txt", sep = '\t', quote = FALSE, col.names = NA)
-total2<-read.table("total.txt",sep="\t",header=T, check.names=FALSE)#????????tumor??normal????Ϣ
-total_tumor<-subset(total2,class=="tumor")
-#survival package
-library(survival)
-fit1<-survfit(Surv(total_tumor$cli.X_OS,total_tumor$cli.X_OS_IND)~1)
-plot(fit1,main="Kaplan-Meier curve,",xlab="time",ylab="probability")
-#estimate the cumulative hazard function
-temp1<-summary(survfit(Surv(total_tumor$cli.X_OS,total$cli.X_OS_IND)~1,type="fh"))
-list(temp1$time,-log(temp1$surv))
-temp1$surv
 
-#cox regression model
-#compute beta
-coxfit1<-coxph(Surv(cli.X_OS,cli.X_OS_IND)~class,data=total_tumor)#?ο?coxfit1<-coxph(Surv(time,status)~x,data=aml)
-summary(coxfit1)
-#obtain the cumulative baseline hazard estimator
-basehaz(coxph(Surv(cli.X_OS,cli.X_OS_IND)~class,data=total_tumor))
-#obtain the survival function
-survfit(coxfit1,newdata=data.frame(total_tumor$class=1))
 
 
